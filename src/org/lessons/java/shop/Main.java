cffe6a574e83426da98b225b07dfc7abb4c7e8e9
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-13 15:48:03 +0100

    settati get e set

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,5 +1,17 @@
 package org.lessons.java.shop;
 
+
+
 public class Main {
+	
+	public static void main (String[] args) {
+		
+		Prodotto p1= new Prodotto(1, "pc", "è un pc", 350f, 22);
+		
+		
+		
+		System.out.println(p1);
+		
+	}
 
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,90 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+	//dichiaro le variabili
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private int iva;
+	
+	
+	//creo il costruttore
+	public Prodotto(int codice, String nome, String descrizione, float prezzo, int iva) {
+		 setCodice(codice);
+		 setNome(nome);
+		 setDescrizione(descrizione);
+		 setPrezzo(prezzo);
+		 setIva(iva);
+	}
+	
+	//metodo get e set
+	
+	public int getCodice() {
+		return codice;
+	}
+	
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+	   // Metodo getter e setter per il nome
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Metodo getter e setter per la descrizione
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    // Metodo getter e setter per il prezzo
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    // Metodo getter e setter per l'IVA
+    public int getIva() {
+        return iva;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String toString() {
+		return "Prodotto:\n"
+				+ "codice:" + " " + getCodice() + "\n"
+				+ "nome:" + " " + getNome() + "\n"
+				+ "descrizione:" + " " + getDescrizione() + "\n"
+				+ "prezzo:" + " " + getPrezzo() + "euro" + "\n"
+				+ "iva:" + " " + getIva() + "%\n";
+				
+	}
+	
 
 }
